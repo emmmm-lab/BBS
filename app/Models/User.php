d753fa84e7f80b9511b0b0d10055383ba6530195
吴瑞鹏 <137584321@qq.com>
2020-02-29 12:14:11 +0000

    回复数据

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,4 +31,9 @@ class User extends Authenticatable implements MustVerifyEmailContract
     {
         return $this->id == $model->user_id;
     }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
