f413a14779994c4074bc425201e2dadf5cc1b0b3
吴瑞鹏 <137584321@qq.com>
2020-03-01 05:41:56 +0000

    消息通知列表

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,5 @@ Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload
 Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
 
 Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
+
+Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
